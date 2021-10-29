500b96b8e6415d4871017733e4f0da846324bb50
TayMcKenzieSA <taylorocarroll@gmail.com>
2021-10-29 16:52:14 +1300

    Fixed Indentation

--- Config.lua
+++ Config.lua
@@ -1,50 +1,50 @@
 Config = {
--- Change the language of the menu here!.
--- Note fr and de are google translated, if you would like to help out with translation / just fix it for your server check below and change translations yourself
--- try en, fr, de or sv.
-	MenuLanguage = 'en',	
--- Set this to true to enable some extra prints
+	-- Change the language of the menu here!.
+	-- Note fr and de are google translated, if you would like to help out with translation / just fix it for your server check below and change translations yourself
+	-- try en, fr, de or sv.
+	MenuLanguage = 'en',
+	-- Set this to true to enable some extra prints
 	DebugDisplay = false,
--- Set this to false if you have something else on X, and then just use /e c to cancel emotes.
+	-- Set this to false if you have something else on X, and then just use /e c to cancel emotes.
 	EnableXtoCancel = true,
--- Set this to true if you want to disarm the player when they play an emote.
+	-- Set this to true if you want to disarm the player when they play an emote.
 	DisarmPlayer= false,
--- Set this if you really wanna disable emotes in cars, as of 1.7.2 they only play the upper body part if in vehicle
-    AllowedInCars = true,
--- You can disable the (F3) menu here / change the keybind.
+	-- Set this if you really wanna disable emotes in cars, as of 1.7.2 they only play the upper body part if in vehicle
+	AllowedInCars = true,
+	-- You can disable the (F3) menu here / change the keybind.
 	MenuKeybindEnabled = true,
 	MenuKeybind = 170, -- (170 is the F3 key, if you Would prefer to use F5, use '166' instead) - Get the button number here https://docs.fivem.net/game-references/controls/
--- You can disable the Favorite emote keybinding here.
+	-- You can disable the Favorite emote keybinding here.
 	FavKeybindEnabled = true,
 	FavKeybind = 171, -- Get the button number here https://docs.fivem.net/game-references/controls/
--- You can change the header image for the f3 menu here
--- Use a 512 x 128 image!
--- NOte this might cause an issue of the image getting stuck on peoples screens
+	-- You can change the header image for the f3 menu here
+	-- Use a 512 x 128 image!
+	-- NOte this might cause an issue of the image getting stuck on peoples screens
 	CustomMenuEnabled = false,
 	MenuImage = "https://imgur.com/PvMINNO.png",
--- You can change the menu position here
+	-- You can change the menu position here
 	MenuPosition = "right", -- (left, right)
--- You can disable the Ragdoll keybinding here.
+	-- You can disable the Ragdoll keybinding here.
 	RagdollEnabled = true,
 	RagdollKeybind = 303, -- Get the button number here https://docs.fivem.net/game-references/controls/
--- You can disable the Facial Expressions menu here.
+	-- You can disable the Facial Expressions menu here.
 	ExpressionsEnabled = true,
--- You can disable the Walking Styles menu here.
-	WalkingStylesEnabled = true,	
--- You can disable the Shared Emotes here.
-    SharedEmotesEnabled = true,
-    CheckForUpdates = true,
--- If you have the SQL imported enable this to turn on keybinding.
-    SqlKeybinding = true,
+	-- You can disable the Walking Styles menu here.
+	WalkingStylesEnabled = true,
+	-- You can disable the Shared Emotes here.
+	SharedEmotesEnabled = true,
+	CheckForUpdates = true,
+	-- If you have the SQL imported enable this to turn on keybinding.
+	SqlKeybinding = true,
 }
 
 Config.KeybindKeys = {
-    ['num4'] = 108,
-    ['num5'] = 110,
-    ['num6'] = 109,
-    ['num7'] = 117,
-    ['num8'] = 111,
-    ['num9'] = 118
+	['num4'] = 108,
+	['num5'] = 110,
+	['num6'] = 109,
+	['num7'] = 117,
+	['num8'] = 111,
+	['num9'] = 118
 }
 
 Config.Languages = {
