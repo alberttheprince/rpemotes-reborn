eec647ca217261057ab674544d2606b0de6ad7b0
TayMcKenzieNZ <49776183+TayMcKenzieNZ@users.noreply.github.com>
2021-12-05 17:54:03 +1300

    Updated fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,14 @@
+--- DPEmotes by andristum ---
+--- Forked by TayMcKenzieNZ ---
+--- Check for updates at https://github.com/TayMcKenzieNZ/dpemotes ---
+
+
 fx_version 'adamant'
 
 game 'gta5'
 
 -- Comment this out if you don't want to use the SQL keybinds
-dependency 'ghmattimysql'
+-- dependency 'ghmattimysql'
 
 client_scripts {
 	'NativeUI.lua',
@@ -15,3 +20,6 @@ server_scripts {
 	'Config.lua',
 	'Server/*.lua'
 }
+
+
+server_script 'printer.lua'
