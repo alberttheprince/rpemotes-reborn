be3e13d920490e486c0a4831b442a2e49b9c2ec5
AvaN0x <clement.ricatte57@gmail.com>
2022-03-25 12:08:17 +0100

    style: :art: format

--- client/Keybinds.lua
+++ client/Keybinds.lua
@@ -73,17 +73,17 @@ if Config.SqlKeybinding then
     AddEventHandler("dp:ClientKeybindGetOne", function(key, e)
         SimpleNotify(Config.Languages[lang]['bound'] .. "~y~" .. e .. "~w~ " .. Config.Languages[lang]['to'] .. " ~g~" .. firstToUpper(key) .. "~w~")
         if key == "num4" then emob1 = e
-        keyb1 = "num4"
+            keyb1 = "num4"
         elseif key == "num5" then emob2 = e
-        keyb2 = "num5"
+            keyb2 = "num5"
         elseif key == "num6" then emob3 = e
-        keyb3 = "num6"
+            keyb3 = "num6"
         elseif key == "num7" then emob4 = e
-        keyb4 = "num7"
+            keyb4 = "num7"
         elseif key == "num8" then emob5 = e
-        keyb5 = "num8"
+            keyb5 = "num8"
         elseif key == "num9" then emob6 = e
-        keyb6 = "num9"
+            keyb6 = "num9"
         end
     end)
 
