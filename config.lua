41e8ca00186ff5bf8ad639d28d04ea14ceebd6ee
TayMcKenzieSA <taylorocarroll@gmail.com>
2023-01-17 17:48:24 +1300

    🥳 Added RPEmotes Banner For Jan 2023 🥳

--- config.lua
+++ config.lua
@@ -26,7 +26,7 @@ Config = {
     -- Use a 512 x 128 image!
     -- Note this might cause an issue of the image getting stuck on peoples screens
     CustomMenuEnabled = true,
-    MenuImage = "https://i.imgur.com/5QFPrPO.png", ----[Custom banner IMGUR or GIPHY URLs go here ]---
+    MenuImage = "https://i.giphy.com/media/kL2WStvEEokt0MioqC/giphy.gif", ----[Custom banner IMGUR or GIPHY URLs go here ]---
     -- You can change the menu image by pasting a link above. It must be the same width and length
     -- You can change the name of the menu here or leave it blank to remove the text in front of the custom banners
     MenuTitle = "",
