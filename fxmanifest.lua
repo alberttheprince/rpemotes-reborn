240038d9338fac6d3777087c4728997554ca7c15
TayMcKenzieSA <taylorocarroll@gmail.com>
2023-01-29 23:07:10 +1300

    🧹 Clean Up 🧹

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -48,47 +48,8 @@ client_scripts {
 }
 
 
----- These are required for the custom props to load. If you have custom props, add data_file 'DLC_ITYP_REQUEST' 'custompropytypnamehere.ytyp' below ---------
+---- Loads all ytyp files for custom props to stream ---
 
-data_file 'DLC_ITYP_REQUEST' 'badge1.ytyp'
-
-data_file 'DLC_ITYP_REQUEST' 'copbadge.ytyp'
-
-data_file 'DLC_ITYP_REQUEST' 'prideprops_ytyp'
-
-data_file 'DLC_ITYP_REQUEST' 'lilflags_ytyp'
-
-data_file 'DLC_ITYP_REQUEST' 'bzzz_foodpack'
-
-data_file 'DLC_ITYP_REQUEST' 'bzzz_prop_torch_fire001.ytyp'
-
-data_file 'DLC_ITYP_REQUEST' 'natty_props_lollipops.ytyp'
-
-data_file 'DLC_ITYP_REQUEST' 'apple_1.ytyp'
-
-data_file 'DLC_ITYP_REQUEST' 'stream/bzzz_food_icecream_pack.ytyp'
-
-data_file 'DLC_ITYP_REQUEST' 'stream/bzzz_food_dessert_a.ytyp'
-
-data_file 'DLC_ITYP_REQUEST' 'stream/bzzz_prop_give_gift.ytyp'
-
-data_file 'DLC_ITYP_REQUEST' 'stream/ultra_ringcase.ytyp'
-
-data_file 'DLC_ITYP_REQUEST' 'stream/bzzz_food_xmas22.ytyp'
-
-data_file 'DLC_ITYP_REQUEST' 'stream/knjgh_pizzas.ytyp'
-
-data_file 'DLC_ITYP_REQUEST' 'stream/pata_christmasfood.ytyp'
-
-data_file 'DLC_ITYP_REQUEST' 'stream/bzzz_prop_cake_love_001.ytyp'
-
-data_file 'DLC_ITYP_REQUEST' 'stream/bzzz_prop_cake_birthday_001.ytyp'
-
-data_file 'DLC_ITYP_REQUEST' 'stream/bzzz_prop_cake_baby_001.ytyp'
-
-data_file 'DLC_ITYP_REQUEST' 'stream/bzzz_prop_cake_casino001.ytyp'
-
-data_file 'DLC_ITYP_REQUEST' 'stream/taymckenzienz_skateboards.ytyp'
-
-data_file 'DLC_ITYP_REQUEST' 'stream/brum_heart.ytyp'
+data_file 'DLC_ITYP_REQUEST' '*.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/*.ytyp'
 
