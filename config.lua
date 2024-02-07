8079150d6964271bc4f6d244adbbb5fa4489836a
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2024-02-07 18:46:27 -0500

    reorganize config to be more tidy

--- config.lua
+++ config.lua
@@ -1,101 +1,86 @@
---- Maintained by TayMcKenzieNZ for the community ---
---- Leakers and resellers are the absolute scum of the earth ---
---- RPEMOTES IS AND ALWAYS WILL BE FREE. STOP PAYING FOR LEAKS!!! ---
+--- Formerly maintained by TayMcKenzieNZ, now updated by Popcorn RP
 
 Config = {
-    -- Change the language of the menu here!.
-    -- Thank you to those who provided translations.
-    -- If you would like to provide translations or help fix them, join our Discord!
-    -- Check languages code below to change the menu language
-    MenuLanguage = 'en',
-    -- Set this to true to enable some extra prints
-    DebugDisplay = false,
-    -- Set this to false if you have something else on X, and then just use /e c to cancel emotes.
-    EnableCancelKeybind = true,
+    -- Menu Options
+    MenuLanguage = 'en', -- Change the language of the menu here!.
+    DebugDisplay = false, -- Set this to true to enable some extra prints
+    EnableCancelKeybind = true,  -- Set this to false if you have something else on X, and then just use /e c to cancel emotes.
     CancelEmoteKey = 'x', -- Get the button string here https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
-    -- Set this to true if you want to disarm the player when they play an emote.
-    DisarmPlayer = false,
-    -- Set this to true if you want to allow people punching while emotes are playing
-    AllowPunching = false,
-    -- Set this if you really wanna disable emotes in cars, as of 1.7.2 they only play the upper body part if in vehicle
-    AllowedInCars = true,
-    -- You can disable the menu here / change the keybind. It is currently set to F4
-    -- The keybind you set here will be server sided for new players. existing players can set their own in the ESC menu keybind settings
     MenuKeybindEnabled = true,
     MenuKeybind = 'f4', -- Get the button string here https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
-    -- You can disable the Favorite emote keybinding here.
-    FavKeybindEnabled = true,
+    FavKeybindEnabled = true, -- You can disable the Favorite emote keybinding here.
     FavKeybind = 'capital', -- Get the button string here https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
-    -- You can change the header image for the menu here
+    SqlKeybinding = false, -- If you have the SQL imported enable this to turn on keybinding.
+    NotificationsAsChatMessage = false, -- If you don't like native GTA 5 notifications, you can disable them here to have messages in the chat.
+    Framework = `qb-core`, -- Used for few framework-dependent things. Accepted values: `qb-core`, false
+    Search = true, -- Used to enable or disable the search feature in the menu.
+    CancelPreviousEmote = true,     -- If turned on, playing an emote will cancel the previous one.
+
+    -- Menu Customization Options
+    MenuTitle = ``, -- You can change the name of the menu here or leave it blank to remove the text in front of the custom banners
+    MenuPosition = `right`, -- You can change the menu position here. Use `left` or `right`
+    CustomMenuEnabled = true, -- You can change the header image for the menu here, Note this might cause an issue of the image getting stuck on people's screens
+    MenuImage = `https://i.imgur.com/FvIgTMP.png`, -- You can change the menu image here, please use an image hosted on Imgur or GIPHY with the same width and length
     -- Use a 512 x 128 image!
-    -- Note this might cause an issue of the image getting stuck on peoples screens
-    CustomMenuEnabled = true,
-    MenuImage = "https://i.imgur.com/FvIgTMP.png", ----[Custom banner IMGUR or GIPHY URLs go here ]---
-    -- You can change the menu image by pasting a link above. It must be the same width and length
-    -- You can change the name of the menu here or leave it blank to remove the text in front of the custom banners
-    MenuTitle = "",
-    -- You can change the menu position here
-    MenuPosition = "right", -- (left, right)
-    -- You can enable or disable the Ragdoll keybinding here.
-    RagdollEnabled = false,
+    
+    -- Combat Car, and Player Movement Options
+    DisarmPlayer = false, -- Set this to true if you want to disarm the player when they play an emote.
+    AllowPunching = false, -- Set this to true if you want to allow people punching while emotes are playing
+    AllowedInCars = true, -- Set this if you really wanna disable emotes in cars, as of 1.7.2 they only play the upper body part if in a vehicle
+    -- You can disable the menu here / change the keybind. It is currently set to F4
+    -- The keybind you set here will be server-sided for new players. existing players can set their own in the ESC menu keybind settings
+    AllowInWater = false, -- If turned off, opening the menu and playing an emote will not be possible while swimming
+
+    -- Ragdoll options
+    RagdollEnabled = false,     -- You can enable or disable the Ragdoll keybinding here.
     RagdollKeybind = 'u', -- Get the button string here https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
     -- You can enable or disable Ragdoll as Toggle here. Only works if RagdollEnabled is set to true.
     RagdollAsToggle = true,
-    -- You can disable the Facial Expressions menu here.
-    ExpressionsEnabled = true,
-    -- Saves expression style to client and applies them on joining
-    PersistentExpression = true,
-    -- You can disable the Walking Styles menu here.
-    WalkingStylesEnabled = true,
-    -- Saves walk styles to client and applies them on joining
-    PersistentWalk = true,
-    -- You can disable the Shared Emotes here.
-    SharedEmotesEnabled = true,
-    -- If you have the SQL imported enable this to turn on keybinding.
-    SqlKeybinding = false,
-    -- If you don't like gta notifications, you can disable them here to have messages in the chat.
-    NotificationsAsChatMessage = false,
-    -- Used for few framework dependent things. Accepted values: "qb-core", false
-    Framework = false,
-    -- You can disable the Adult Emotes here.
-    AdultEmotesDisabled = false,
-    -- You can disable the Animal Emotes here.
-    AnimalEmotesEnabled = true,
-    -- Used to enable or disable the search feature in the menu.
-    Search = true,
-    -- You can disable the handsup here / change the keybind. It is currently set to Y
-    HandsupEnabled = false,
+    
+    -- Expressions, Walks, and More Menu Options
+    ExpressionsEnabled = true, -- You can disable the Facial Expressions menu here. 
+    PersistentExpression = true, -- Saves expression style to client and applies them on joining
+    WalkingStylesEnabled = true, -- You can disable the Walking Styles menu here.
+    PersistentWalk = true, -- Saves walk styles to client and applies them on joining
+    SharedEmotesEnabled = true, -- You can disable the Shared Emotes here.
+    AdultEmotesDisabled = false, -- You can disable the Adult Emotes here.
+    AnimalEmotesEnabled = true, -- You can disable the Animal Emotes here.
+    
+    -- Hands Up Options 
+    HandsupEnabled = false, -- You can disable the handsup here / change the keybind. It is currently set to Y
     HandsupKeybind = 'Y', -- Get the button string here https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
     HandsupKeybindEnabled = false,
     HandsupKeybindInCarEnabled = false,
     PersistentEmoteAfterHandsup = true, -- If true, you will play the emote you were playing previously after you stop handsup.
-    -- You can disable the fingrer pointing here / change the keybind. It is currently set to B
-    PointingEnabled = false,
+    
+    -- Pointing Options
+    PointingEnabled = true, -- You can disable the finger pointing here / change the keybind. It is currently set to B
     PointingKeybindEnabled = true,
     PointingKeybind = 'B', -- Get the button string here https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
     PointingKeybindInCarEnabled = false,
     PersistentEmoteAfterPointing = true, -- If true, you will play the emote you were playing previously after you stop pointing.
-    -- If crouching should be enabled.
-    CrouchEnabled = true,
+    
+    -- Crouching Options
+    CrouchEnabled = true, -- If crouching should be enabled.
     CrouchKeybindEnabled = true, -- If true, crouching will use keybinds.
     CrouchKeybind = 'lcontrol', -- The default crouch keybind, get the button string here: https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
-    CrouchOverride = false, -- If true, you won't enter stealth mode even if the crouch key and the "duck" key are the same.
-    -- If crawling should be enabled.
-    CrawlEnabled = true,
+    CrouchOverride = false, -- If true, you won't enter stealth mode even if the crouch key and the `duck` key are the same.
+    FpsMode = false, -- set this to true if you have first-person shooting to disable the ability to crouch and shoot in third-person
+    
+    -- Crawling Options
+    CrawlEnabled = true, -- If crawling should be enabled.
     CrawlKeybindEnabled = true, -- If true, crawling will use keybinds.
     CrawlKeybind = 'rcontrol', -- The default crawl keybind, get the button string here: https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
-    -- If turned on, playing an emote will cancel the previous one.
-    CancelPreviousEmote = false,
-    -- If turned off, opening the menu and playing an emote will not be possible while swimming
-    AllowInWater = true,
-    -- If set to true, the /binoculars command will be enabled.
-    BinocularsEnabled = true,
-    -- If set to true, you'll be able to toggle between different vision modes in the binoculars
-    AllowVisionsToggling = true,
-    -- If set to true, the /newscam command will be enabled.
-    NewscamEnabled = true,
-    -- Check for updates
-    CheckForUpdates = true,
+
+    -- Binocular Options
+    BinocularsEnabled = false, -- If set to true, the /binoculars command will be enabled.
+    AllowVisionsToggling = false, -- If set to true, you'll be able to toggle between different vision modes in the binoculars
+    
+    -- News Camera Options
+    NewscamEnabled = false, -- If set to true, the /newscam command will be enabled.
+    
+    -- Misc
+    CheckForUpdates = false, -- Please keep false, as the emote menu as this no longer checks for updates
 }
 
 Config.KeybindKeys = {
