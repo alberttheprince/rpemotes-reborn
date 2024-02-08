40bd1161af9571de8409cc6c23baf6e2c7ae3094
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2024-02-08 07:15:19 -0500

    removed unnecessary print

--- printer.lua
+++
@@ -1,9 +0,0 @@
-print("RpEmotes by TayMcKenzieNZ, Mathu_lmn & MadsL loaded sucessfully.")
-
-SetTimeout( 1000, function ()
-    if GetResourceState('dpemotes') == 'started' then
-        for i = 1, 10 do
-            print("RpEmotes: Warning! dpemotes is on the server, this WILL cause issues.")
-        end
-    end
-end)
