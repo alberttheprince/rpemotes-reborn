1e705a56e6ffdf0ad7ee1bded90bbf265e2a3708
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2024-07-07 00:41:30 -0400

    Update NoIdleCam.lua

--- client/NoIdleCam.lua
+++ client/NoIdleCam.lua
@@ -1,9 +1,3 @@
---- RPEmotes by TayMcKenzieNZ, Mathu_lmn and MadsL, maintained by TayMcKenzieNZ ---
---- Download OFFICIAL version and updates ONLY at https://github.com/TayMcKenzieNZ/rpemotes ---
---- RPEmotes is FREE and ALWAYS will be. STOP PAYING SCAMMY FUCKERS FOR SOMEONE ELSE'S WORK!!! ---
-
-
-
 RegisterCommand('idlecamoff', function() -- help2 31, 167, 9
   TriggerEvent('chat:addMessage', {
     color = {227,8,0},
