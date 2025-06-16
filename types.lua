b5f78c9273eb9cd12686351217d199a3d95f712e
Manason <clanerp@gmail.com>
2025-06-16 09:41:22 -0700

    chore: add typing to animation list (#118)

---
+++ types.lua
@@ -0,0 +1,76 @@
+---@meta
+
+---@alias Dictionary string
+---@alias AnimName string
+---@alias ScenarioName string
+---@alias Label string
+
+---@class Color
+---@field R number
+---@field G number
+---@field B number
+---@field A number
+
+---@class AnimationOptions
+---@field EmoteMoving? boolean
+---@field EmoteLoop? boolean
+---@field EmoteStuck? boolean
+---@field FullBody? boolean
+---@field Attachto? boolean
+---@field NotInVehicle? boolean
+---@field onlyInVehicle? boolean
+---@field EmoteDuration? integer
+---@field SyncOffsetFront? number
+---@field SyncOffsetSide? number
+---@field SyncOffsetHeading? number
+---@field bone? integer
+---@field xPos? number
+---@field yPos? number
+---@field zPos? number
+---@field xRot? number
+---@field yRot? number
+---@field zRot? number
+---@field Prop? string
+---@field PropBone? integer
+---@field PropPlacement? number[]
+---@field PropNoCollision? boolean
+---@field StartDelay? integer
+---@field SecondProp? string
+---@field SecondPropBone? integer
+---@field SecondPropPlacement? number[]
+---@field SecondPropNoCollision? boolean
+---@field PropTextureVariations? {Name: string, Value: integer}[]
+---@field PtfxAsset? string
+---@field PtfxName? string
+---@field PtfxNoProp? boolean
+---@field PtfxPlacement? number[]
+---@field PtfxInfo? string
+---@field PtfxWait? number
+---@field PtfxCanHold? boolean
+---@field PtfxColor? Color[]
+---@field PtfxBone? integer
+---@field ExitEmote? string
+---@field ExitEmoteType? "Exits"
+---@field BlendInSpeed? number
+---@field BlendOutSpeed? number
+---@field Flag? integer
+
+---@class AnimationListConfig
+---@field Expressions table<string, {[1]: AnimName, [2]: Label?}>
+---@field Walks table<string, {[1]: AnimName, [2]: Label?}>
+---@field Shared table<string, {[1]: Dictionary, [2]: AnimName, [3]: Label, [4]: AnimName?, AnimationOptions?: AnimationOptions, AnimalEmote?: boolean}>
+---@field Dances table<string, {[1]: Dictionary, [2]: AnimName, [3]: Label, AnimationOptions?: AnimationOptions}>
+---@field AnimalEmotes table<string, {[1]: Dictionary, [2]: AnimName, [3]: Label, AnimationOptions?: AnimationOptions, AdultAnimation?: boolean, AnimalEmote?: boolean}>
+---@field Exits table<string, {[1]: Dictionary, [2]: AnimName, [3]: Label, AnimationOptions?: AnimationOptions}>
+---@field Emotes table<string, {[1]: Dictionary | 'MaleScenario' | 'Scenario', [2]: AnimName | ScenarioName, [3]: Label, AnimationOptions?: AnimationOptions, AdultAnimation?: boolean}>
+---@field PropEmotes table<string, {[1]: Dictionary, [2]: AnimName, [3]: Label, AnimationOptions?: AnimationOptions}>
+
+---@class EmoteData
+---@field [1] AnimName | Dictionary | 'MaleScenario' | 'Scenario' | 'ScenarioObject'
+---@field [2] AnimName | ScenarioName | Label?
+---@field [3]? Label
+---@field [4]? AnimName Second player's anim during a shared emote. Defaults to the same as first player if unset.
+---@field AnimationOptions? AnimationOptions
+---@field AnimalEmote? boolean
+---@field AdultAnimation? boolean
+---@field category 'Expressions' | 'Walks' | 'Shared' | 'Dances' | 'AnimalEmotes' | 'Exits' | 'Emotes' | 'PropEmotes'
