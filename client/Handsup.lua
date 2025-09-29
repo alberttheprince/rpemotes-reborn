f7d771def1a1c2ba1c84fd6850bc5ae875906bbd
bedoflex <101467711+bedoflex@users.noreply.github.com>
2025-09-29 11:47:54 +0300

    Update Handsup.lua

--- client/Handsup.lua
+++ client/Handsup.lua
@@ -17,10 +17,7 @@ local function HandsUpLoop()
 
             if not IsEntityPlayingAnim(PlayerPedId(), "random@mugging3", "handsup_standing_base", 49) then
                 ClearPedSecondaryTask(PlayerPedId())
-                CreateThread(function()
-                    Wait(350)
-                    InHandsup = false
-                end)
+                InHandsup = false
             end
 
             Wait(0)
