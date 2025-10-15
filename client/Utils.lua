33b566ef7e3e1daf1030867cf9d56e3a44a7d14e
Christopher M. <mrcm8263@gmail.com>
2025-10-15 17:22:00 +1100

    Refactors ClonedPed to remove idle animations

--- client/Utils.lua
+++ client/Utils.lua
@@ -366,6 +366,7 @@ function ShowPedMenu(zoom)
                 local heading_offset = Config.MenuPosition == "left" and 170.0 or 190.0
                 SetEntityHeading(ClonedPed, camRot.z + heading_offset)
                 SetEntityRotation(ClonedPed, camRot.x * (-1), 0.0, camRot.z + 170.0, 2, false)
+                ForcePedMotionState(ClonedPed, `MotionState_None`, false, 1, true)
 
                 Wait(4)
             end
